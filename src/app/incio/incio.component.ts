5c5bb361d6091f532543034b83b709d2192192da
abraham1309 <superx1@live.com.mx>
2021-06-21 00:31:14 -0500

    Se agrego el autor de la frase, se corriguio algunos errores

--- src/app/incio/incio.component.ts
+++ src/app/incio/incio.component.ts
@@ -16,12 +16,29 @@ export class IncioComponent implements OnInit {
     const text="si se puede imaginar se puede programar";
     var textOb:any;
 
+    var aux2=0;
+    const text2="En memoria de Alejandro Taboada Sánchez (Programacion ATS)";
+    var textOb:any;
+
     function escribir() {
       if(i<text.length){
         textOb= document.getElementById('divText');
         textOb.innerHTML +=text.charAt(i);
         i++;
         setTimeout(escribir, 70);
+      }
+
+      if(i==text.length){
+        setTimeout(escribir2, 900);
+      }
+    }
+
+    function escribir2() {
+      if(aux2<text2.length){
+        textOb= document.getElementById('divText2');
+        textOb.innerHTML +=text2.charAt(aux2);
+        aux2++;
+        setTimeout(escribir2, 140);
       }
     }
 
