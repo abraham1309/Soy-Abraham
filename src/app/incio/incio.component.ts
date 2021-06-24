d8f20fd730e0c360d46db66fb8f0f3504e9a7cb2
abraham1309 <superx1@live.com.mx>
2021-06-23 23:27:08 -0500

    Se modifico la pagina par aque se pudiera ver en el navegador de fb

--- src/app/incio/incio.component.ts
+++ src/app/incio/incio.component.ts
@@ -14,16 +14,21 @@ export class IncioComponent implements OnInit {
     //Funcion para la animacion de escribir 
     var i=0;
     const text="si se puede imaginar se puede programar";
-    var textOb:any;
+    var textOb1:any;
+    var textOb12: any;
 
     var aux2=0;
     const text2="En memoria de Alejandro Taboada Sánchez (Programacion ATS)";
-    var textOb:any;
+    var textOb2:any;
+    var textOb22:any;
 
     function escribir() {
       if(i<text.length){
-        textOb= document.getElementById('divText');
-        textOb.innerHTML +=text.charAt(i);
+        textOb1= document.getElementById('divText1.2');
+        textOb1.innerHTML +=text.charAt(i);
+
+        textOb12=document.getElementById('divText1.1');
+        textOb12.innerHTML +=text.charAt(i);
         i++;
         setTimeout(escribir, 70);
       }
@@ -35,8 +40,11 @@ export class IncioComponent implements OnInit {
 
     function escribir2() {
       if(aux2<text2.length){
-        textOb= document.getElementById('divText2');
-        textOb.innerHTML +=text2.charAt(aux2);
+        textOb2= document.getElementById('divText2.2');
+        textOb2.innerHTML +=text2.charAt(aux2);
+        
+        textOb22=document.getElementById('divText2.1');
+        textOb22.innerHTML +=text2.charAt(aux2);
         aux2++;
         setTimeout(escribir2, 140);
       }
