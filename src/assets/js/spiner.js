d5d8aa95636934f14ed64aa992a70073c85ed78e
abraham1309 <superx1@live.com.mx>
2021-12-25 18:16:52 -0600

    Se agrego un nuevo proyecto y spiner

--- src/assets/js/spiner.js
+++ src/assets/js/spiner.js
@@ -1,14 +1,15 @@
 (function($) {
-    "use strict"; 
-	
-	$(window).on('load', function() {
-		var preloaderFadeOutTime = 500;
-		function hidePreloader() {
-			var preloader = $('.spinner-wrapper');
-			setTimeout(function() {
-				preloader.fadeOut(preloaderFadeOutTime);
-			}, 500);
-		}
-		hidePreloader();
-	});
+    "use strict";
+
+    $(window).on('load', function() {
+        var preloaderFadeOutTime = 500;
+
+        function hidePreloader() {
+            var preloader = $('.spinner-wrapper');
+            setTimeout(function() {
+                preloader.fadeOut(preloaderFadeOutTime);
+            }, 4560);
+        }
+        hidePreloader();
+    });
 })(jQuery);
